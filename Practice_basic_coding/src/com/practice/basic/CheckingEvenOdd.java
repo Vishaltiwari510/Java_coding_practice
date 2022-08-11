be52e3d5cf2208e12f2555441a3d8e593103aad2
Vishaltiwari510 <tiwarivishal183@gmail.com>
2022-08-11 15:15:11 +0530

    second code

---
+++ Practice_basic_coding/src/com/practice/basic/CheckingEvenOdd.java
@@ -0,0 +1,21 @@
+package com.practice.basic;
+
+import java.util.Scanner;
+
+public class CheckingEvenOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  Scanner sc = new Scanner(System.in);
+	       System.out.println("Enter the Number");
+	       int num = sc.nextInt();
+	       if(num%2==0) {
+	    	   System.out.println("Even Number");
+	       }
+	       else  {
+	    	   System.out.println("Odd Number");
+	       }
+	      
+	}
+
+}
